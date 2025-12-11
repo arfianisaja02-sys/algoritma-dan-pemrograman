57a8e36ba2ceea23dc73fb69400b7fbab07b618a
arfianisaja02-sys <arfianisaja02@gmail.com>
2025-12-11 19:59:26 +0800

    Create 1. Tipe Data  array Satu Dimensi, Dua Dimansi, dan Tiga Dimensi.py

---
+++ final test/1. Tipe Data  array Satu Dimensi, Dua Dimansi, dan Tiga Dimensi.py
@@ -0,0 +1,25 @@
+# array 1dimensi 
+array1D = [10, 30, 50, 60, 70]
+
+
+
+#2 dimensi
+array2D = [
+    [1, 2, 3,4],
+    [5, 6, 7,8],
+    [9, 10, 11,12]
+]
+
+array3D = [
+    [
+        [5, 6], 
+        [7, 8]
+    ],
+    [
+        [1, 2],
+        [3, 4]
+    ]
+]
+print(array3D)
+print(array1D)
+print(array2D)
